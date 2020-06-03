38f9eab8532f67bc5f811864b16ed3b57ff533b2
Luis Pérez <lapv2991@gmail.com>
2020-06-03 18:37:25 -0500

    Instalando Firebase y AngularFire2, Seteando cuenta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase, 'babaria-webpage'),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
     AppRoutingModule
   ],
   providers: [],
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBPEDytmU1tZ9G-y9j2PlMbr0Ta540seLM',
+    authDomain: 'jonathan-webpage.firebaseapp.com',
+    databaseURL: 'https://jonathan-webpage.firebaseio.com',
+    projectId: 'jonathan-webpage',
+    storageBucket: 'jonathan-webpage.appspot.com',
+    messagingSenderId: '923753377372',
+    appId: '1:923753377372:web:1d4f981093997ad7798099',
+    measurementId: 'G-YB47LS8HZJ'
+  };
 };
